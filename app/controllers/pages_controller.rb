8bd1692a42316856d2d3830a17af7344e4f732dd
Hector Correa <correah2009@gmail.com>
2014-06-23 20:51:03 -0700

    Created home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  	#anything that has to do with the home page
+  end
+
+    def about
+  	#anything that has to do with the home page
+  end
+
+end
